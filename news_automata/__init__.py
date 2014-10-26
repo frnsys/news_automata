185874bd562af953a5de15767452d9b65eba48a4
Francis Tseng <f@frnsys.com>
2014-10-26 17:02:15 +0000

    CORS yeah

--- news_automata/__init__.py
+++ news_automata/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
+from flask.ext.cors import CORS
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 
@@ -8,5 +9,8 @@ app.config.from_object('config')
 
 # Setup the database.
 db = MongoEngine(app)
+
+# CORS so the concierge frontend can access the search endpoint.
+CORS(app)
 
 from . import routes
